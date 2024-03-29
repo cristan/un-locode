5e494890f9cbb7fdd090f09840e3729d5be14ef3
Cristan Meijer <cristan.meijer@gmail.com>
2024-03-29 09:05:43 +0100

    Improve downloading from Nominatim

--- scripts/analyze-and-improve/download-nominatim.js
+++ scripts/analyze-and-improve/download-nominatim.js
@@ -8,22 +8,42 @@ async function start() {
         const entry = csvDatabase[unlocode]
 
         const decimalCoordinates = convertToDecimal(entry.coordinates)
-        if (decimalCoordinates && entry.country === "IT") {
-            console.log(`Downloading ${unlocode}`)
-            const ogNominatimQuery = `https://nominatim.openstreetmap.org/search?format=jsonv2&accept-language=en&addressdetails=1&limit=20&city=${encodeURI(entry.city)}&country=${encodeURI(entry.country)}`
-            let nominatimQuery = ogNominatimQuery
-            const region = entry.subdivisionCode;
-            let byRegion = false
-            if (region) {
-                nominatimQuery += `&state=${encodeURI(region)}`
-                byRegion = true
-            }
+        if (!decimalCoordinates || entry.country !== "CN") {
+            continue
+        }
 
-            let scrapeMethod = byRegion ? "byRegion" : "cityOnly"
-            let directory = `../../data/nominatim/${entry.country}/${entry.location}/${scrapeMethod}`
-            if (!fs.existsSync(directory)){
+        console.log(`Downloading ${unlocode}`)
+        const ogNominatimQuery = `https://nominatim.openstreetmap.org/search?format=jsonv2&accept-language=en&addressdetails=1&limit=20&city=${encodeURI(entry.city)}&country=${encodeURI(entry.country)}`
+        let nominatimQuery = ogNominatimQuery
+        const region = entry.subdivisionCode;
+        let byRegion = false
+        if (region) {
+            nominatimQuery += `&state=${encodeURI(region)}`
+            byRegion = true
+        }
+
+        let scrapeMethod = byRegion ? "byRegion" : "cityOnly"
+        let directory = `../../data/nominatim/${entry.country}/${entry.location}/${scrapeMethod}`
+        if (!fs.existsSync(directory)){
+            fs.mkdirSync(directory, { recursive: true });
+        }
+        const fileName = `${directory}/${unlocode}.json`;
+        const fileAlreadyExists = fs.existsSync(fileName)
+        if (fileAlreadyExists) {
+            console.log(`${fileName} already exists. Skipping.`)
+            continue
+        }
+
+        await delay(1000)
+        const fromNominatim = await (await fetch(nominatimQuery)).text()
+        await fs.writeFileSync(fileName, fromNominatim)
+        if (byRegion && fromNominatim === "[]") {
+            scrapeMethod = "cityOnly"
+            directory = `../../data/nominatim/${entry.country}/${entry.location}/${scrapeMethod}`
+            if (!fs.existsSync(directory)) {
                 fs.mkdirSync(directory, { recursive: true });
             }
+
             const fileName = `${directory}/${unlocode}.json`;
             const fileAlreadyExists = fs.existsSync(fileName)
             if (fileAlreadyExists) {
@@ -32,26 +52,8 @@ async function start() {
             }
 
             await delay(1000)
-            const fromNominatim = await (await fetch(nominatimQuery)).text()
-            await fs.writeFileSync(fileName, fromNominatim)
-            if (byRegion && fromNominatim === "[]") {
-                scrapeMethod = "cityOnly"
-                directory = `../../data/nominatim/${entry.country}/${entry.location}/${scrapeMethod}`
-                if (!fs.existsSync(directory)) {
-                    fs.mkdirSync(directory, { recursive: true });
-                }
-
-                const fileName = `${directory}/${unlocode}.json`;
-                const fileAlreadyExists = fs.existsSync(fileName)
-                if (fileAlreadyExists) {
-                    console.log(`${fileName} already exists. Skipping.`)
-                    continue
-                }
-
-                await delay(1000)
-                const fromNominatim2 = await (await fetch(ogNominatimQuery)).text()
-                await fs.writeFileSync(fileName, fromNominatim2)
-            }
+            const fromNominatim2 = await (await fetch(ogNominatimQuery)).text()
+            await fs.writeFileSync(fileName, fromNominatim2)
         }
     }
 }
