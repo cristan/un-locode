5ba24cde899219bf89613c1a697f16aca0420bc2
Cristan Meijer <cristan.meijer@gmail.com>
2024-04-12 15:17:36 +0200

    Create a script to compare my results with those of wikidata

---
+++ scripts/analyze-and-improve/internal-compare-with-wikidata.js
@@ -0,0 +1,33 @@
+import {readCsv} from "./util/readCsv.js";
+import {convertToDecimal, convertToUnlocode, getDistanceFromLatLonInKm} from "./util/coordinatesConverter.js";
+import {readWikidata} from "./util/wikidata-reader.js";
+
+// Before you run this, make sure to run download-wikidata.js to compare with the latest state at Wikidata
+async function validateAllCoordinates() {
+    const csvDatabase = await readCsv(true)
+    const wikidata = readWikidata();
+
+    console.log()
+
+    let allCount = 0
+    let count = 0
+    for (const unlocode of Object.keys(csvDatabase)) {
+        const entry = csvDatabase[unlocode]
+
+        const decimalCoordinates = convertToDecimal(entry.coordinates)
+        const wikiEntry = wikidata[unlocode];
+        if (!decimalCoordinates || !wikiEntry) {
+            continue
+        }
+
+        const distance = Math.round(getDistanceFromLatLonInKm(decimalCoordinates.latitude, decimalCoordinates.longitude, wikiEntry.lat, wikiEntry.lon))
+        if (distance > 100) {
+            console.log(`https://unlocode.info/${unlocode} ${entry.city} vs ${wikiEntry.itemLabel} ${distance}km distance between https://www.google.com/maps?z=12&ll=${decimalCoordinates.latitude},${decimalCoordinates.longitude} and https://www.google.com/maps?z=12&ll=${wikiEntry.lat},${wikiEntry.lon}. ${unlocode},${convertToUnlocode(wikiEntry.lat, wikiEntry.lon)} Source: ${wikiEntry.item}`)
+            count++
+        }
+        allCount++
+    }
+    console.log(count +" out of "+ allCount +`(${((count / allCount) * 100)}%)`)
+}
+
+validateAllCoordinates()
