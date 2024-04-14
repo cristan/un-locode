f46e885656e1e4e0b7608ece607291d2133bbd86
Cristan Meijer <cristan.meijer@gmail.com>
2024-04-15 00:38:13 +0200

    Make this file test the percentage which has coordinates

--- scripts/analyze-and-improve/internal-validate-region-codes.js
+++ scripts/analyze-and-improve/internal-validate-region-codes.js
@@ -1,33 +1,59 @@
 import {readCsv, readSubdivisionData} from "./util/readCsv.js";
 import {convertToDecimal} from "./util/coordinatesConverter.js";
-import {getNominatimData} from "./util/nominatim-loader.js";
 
 async function validateRegionCodes() {
-    const csvDatabase = await readCsv()
+    const csvDatabase = await readCsv(true)
     const subdivisions = readSubdivisionData()
 
+    let count = 0
+    let allCount = 0
     for (const unlocode of Object.keys(csvDatabase)) {
+        allCount++
+
         const entry = csvDatabase[unlocode]
+
         const decimalCoordinates = convertToDecimal(entry.coordinates)
         if (!decimalCoordinates) {
+            console.log(`https://unlocode.info/${unlocode} ${unlocode} ${entry.city}`)
+            // console.log(entry.iata !== "" ? entry.iata : entry.location)
+            count++
             continue
         }
 
-        const nominatimData = await getNominatimData(entry)
-        if (!nominatimData) {
-            continue
-        }
+        // const city = entry.city
+        // if (city.includes(" Apt") || city.includes("Airport") || city.includes("/") || city.includes(",")) {
+        //     console.log(entry.unlocode +": "+ entry.city)
+        //     const directory = `../../data/nominatim/${entry.country}/${entry.location}`
+        //     fs.rmSync(directory, { recursive: true, force: true });
+        //     count++
+        // }
 
-        nominatimData.result.forEach(nominatimResult => {
-            if (!nominatimResult.subdivisionCode) {
-                return
-            }
-            const subdivisionName = subdivisions[entry.country +"|"+ nominatimResult.subdivisionCode]
-            if (!subdivisionName) {
-                console.log(entry.unlocode, "has no subdivision name for", nominatimResult.subdivisionCode)
-            }
-        })
+        // if (entry.city.includes("/")){
+        //     console.log(`${unlocode}: ${entry.city}`)
+        //     count++
+        // }
+
+        // if (entry.country === "CZ") {
+        //     console.log(`${unlocode}: ${entry.subdivisionCode}`)
+        // }
+
+        // const nominatimData = await getNominatimData(entry)
+        // if (!nominatimData) {
+        //     continue
+        // }
+
+        // nominatimData.result.forEach(nominatimResult => {
+        //     if (!nominatimResult.subdivisionCode) {
+        //         return
+        //     }
+        //     const subdivisionName = subdivisions[entry.country +"|"+ nominatimResult.subdivisionCode]
+        //     if (!subdivisionName) {
+        //         count++
+        //         // console.log(entry.unlocode, "has no subdivision name for", nominatimResult.subdivisionCode)
+        //     }
+        // })
     }
+    console.log(count +" out of "+ allCount +`(${100-((count / allCount) * 100)}%)`)
 }
 
 validateRegionCodes()
