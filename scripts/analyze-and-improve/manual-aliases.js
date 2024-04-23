19c1554daab878531b920d5c571bd63fc65c38b2
Cristan Meijer <cristan.meijer@gmail.com>
2024-04-23 16:28:01 +0200

    Add manual aliases
    It's not used yet, but this is good stuff, so I want it pushed.

---
+++ scripts/analyze-and-improve/manual-aliases.js
@@ -0,0 +1,15 @@
+// Sometimes you basically have the same unlocode twice. In all of these cases, this is a mistake.
+
+export const ALIASES = {
+    AOVPE: "AONGV", // Ongiva => Ondjiva
+    NLBNK: "NLBUK", // Bunnink => Bunnik
+    NLBEW: "NLBLW", // Beiswijk => Bleiswijk
+    ZAGNB: "ZAGAA", // Gans Bay => Gansbaai
+    ARPNL: "ARBHI", // Puerto Nacional/Bahia Blanca => Bahía Blanca
+    ATWDD: "ATWSS", // Weissenstein ob der Drau => Weissenstein
+    AUCAL: "AUCUD", // Caloundra Head => Caloundra
+    AUWEH: "AUWPA", // Wetherhill => Wetherill Park
+    BGGOV: "BGGOZ", // Gorna Oryahovica => Gorna Oryakhovitsa (which should probably be named Gorna Oryahovitsa)
+    BGIPR: "BGISP", // Isperikh => Isperih
+    RUKDT: "RUOKR", // Kronshtadt => Kronstadt
+}
