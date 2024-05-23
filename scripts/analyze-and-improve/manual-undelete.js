b1d0587c345fff75530db78bfcb1599b70c16146
Cristan Meijer <cristan.meijer@gmail.com>
2024-05-23 13:48:34 +0200

    Add another deletion still used in the wild

--- scripts/analyze-and-improve/manual-undelete.js
+++ scripts/analyze-and-improve/manual-undelete.js
@@ -1,5 +1,6 @@
 // Entries which have been deleted by now, but the old (already deleted) entry is still used in the wild
 
 export const DELETIONS_STILL_IN_USE = {
-    CNTAG: "CNTAC" // Taicang Port
+    CNTAG: "CNTAC", // Taicang Port
+    VNCMT: "VNTOT", // Cai Mep
 }
